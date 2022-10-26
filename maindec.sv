fc230c2746cb1a7c045d1aaac0398f16b293c1c7
marisolcastellanos <116708611+marisolcastellanos@users.noreply.github.com>
2022-10-26 15:44:02 -0700

    Update maindec.sv

--- maindec.sv
+++ maindec.sv
@@ -9,18 +9,17 @@ module maindec(input  logic [6:0] op,
   logic [10:0] controls;
 
   //assing the control signals values form the control
-  
+  assign {Regwrite, ImmSrc, ALUSrc, MemWrite, ResultSrc, Branch, ALUOp, Jump}= controls;
 
   always_comb
     case(op)
     // RegWrite_ImmSrc_ALUSrc_MemWrite_ResultSrc_Branch_ALUOp_Jump
-      7'b0000011: controls = RegWrite 1 _ImmSrc 00 _ALUSrc a0 b1 _MemWrite 0 _ResultSrc 01 _Branch 0_ALUOp 00 _Jump 0 // lw
-                             {1,00,0,1,0,01,0,00,0}
-      7'b0100011: controls =  // sw
-      7'b0110011: controls =  // R-type 
-      7'b1100011: controls =  // beq
-      7'b0010011: controls =  // I-type ALU
-      7'b1101111: controls =  // jal
+      7'b0000011: controls =  11'b10010010000;
+      7'b0100011: controls =  11'b00111xx000x;// sw
+      7'b0110011: controls =  11'b1xx00000100;// R-type 
+      7'b1100011: controls =  11'b01000xx1010;// beq
+      7'b0010011: controls =  11'b10000000100;// I-type ALU
+      7'b1101111: controls =  11'b111x0100xx1;// jal
       default:    controls =  // non-implemented instruction
     endcase
 endmodule
