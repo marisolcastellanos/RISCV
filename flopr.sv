66072eec0ff9a44937be9bc990367f6e6a3243bb
marisolcastellanos <116708611+marisolcastellanos@users.noreply.github.com>
2022-10-26 01:30:36 -0700

    Update flopr.sv

--- flopr.sv
+++ flopr.sv
@@ -4,4 +4,9 @@ module flopr #(parameter WIDTH = 8)
                output logic [WIDTH-1:0] q);
 
   // add the logic for the d flip flop with reset
+  
+  always_ff @(posedge clk, posedge reset)
+    if (reset) q<=0;
+    else q<=d;
+  
 endmodule
