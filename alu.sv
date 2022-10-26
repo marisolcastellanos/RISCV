7c80efa9d591ab719e7e054c3abc3aa4a8c039ce
marisolcastellanos <116708611+marisolcastellanos@users.noreply.github.com>
2022-10-26 00:03:30 -0700

    Create alu.sv

---
+++ alu.sv
@@ -0,0 +1,23 @@
+module alu(input  logic [31:0] a, b,
+           input  logic [2:0]  alucontrol,
+           output logic [31:0] result,
+           output logic        zero);
+
+  logic [31:0] condinvb, sum;
+  logic        v;              // overflow
+  logic        isAddSub;       // true when is add or subtract operation
+
+  assign condinvb = alucontrol[0] ? ~b : b;
+  assign sum = a + condinvb + alucontrol[0];
+  assign isAddSub = ~alucontrol[2] & ~alucontrol[1] |
+                    ~alucontrol[1] & alucontrol[0];
+
+  always_comb
+    case (alucontrol)
+      //
+    endcase
+
+  assign zero //
+  assign v = ~(alucontrol[0] ^ a[31] ^ b[31]) & (a[31] ^ sum[31]) & isAddSub;
+  
+endmodule
