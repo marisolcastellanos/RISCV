ba429a8b23a7e0d965b60cecb98c6501cb787239
marisolcastellanos <116708611+marisolcastellanos@users.noreply.github.com>
2022-10-26 00:06:55 -0700

    Create flopr.sv

---
+++ flopr.sv
@@ -0,0 +1,7 @@
+module flopr #(parameter WIDTH = 8)
+              (input  logic             clk, reset,
+               input  logic [WIDTH-1:0] d, 
+               output logic [WIDTH-1:0] q);
+
+  // add the logic for the d flip flop with reset
+endmodule
