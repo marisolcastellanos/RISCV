5b77be46152507a004d5580eeafe07abe9ce1b10
marisolcastellanos <116708611+marisolcastellanos@users.noreply.github.com>
2022-10-26 00:08:08 -0700

    Create mux.sv

---
+++ mux.sv
@@ -0,0 +1,17 @@
+module mux2 #(parameter WIDTH = 8)
+             (input  logic [WIDTH-1:0] d0, d1, 
+              input  logic             s, 
+              output logic [WIDTH-1:0] y);
+
+  //assign statement 
+  
+endmodule
+
+module mux3 #(parameter WIDTH = 8)
+             (input  logic [WIDTH-1:0] d0, d1, d2,
+              input  logic [1:0]       s, 
+              output logic [WIDTH-1:0] y);
+
+  //assign statement 
+  
+endmodule
