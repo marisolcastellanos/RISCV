ab5b9062829b58ffd4911066963e8db0bab12a94
marisolcastellanos <116708611+marisolcastellanos@users.noreply.github.com>
2022-10-26 00:04:20 -0700

    Create aludec.sv

---
+++ aludec.sv
@@ -0,0 +1,15 @@
+module aludec(input  logic       opb5,
+              input  logic [2:0] funct3,
+              input  logic       funct7b5, 
+              input  logic [1:0] ALUOp,
+              output logic [2:0] ALUControl);
+
+  logic  RtypeSub;
+  assign RtypeSub = funct7b5 & opb5;  // TRUE for R-type subtract instruction
+
+  always_comb
+    case(ALUOp)
+     // add the logic here 
+            
+    endcase
+endmodule
