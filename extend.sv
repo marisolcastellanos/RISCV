7d18d8380aee7b9d1e2f09a58317c57da26c15f0
marisolcastellanos <116708611+marisolcastellanos@users.noreply.github.com>
2022-10-28 05:06:11 -0700

    Update extend.sv

--- extend.sv
+++ extend.sv
@@ -5,13 +5,13 @@ module extend(input  logic [31:7] instr,
   always_comb
     case(immsrc) 
                
-      2'b00: immext = 32'  // I-type 
-               // {{20{Instr[31]}}, Instr[31:20]} 
-      2'b01:   // S-type (stores)
+      2'b00: immext = {{20{Instr[31]}}, Instr[31:20]}  // I-type 
                // 
-      2'b10:   // B-type (branches)
+      2'b01: immext =  {{20{Instr[31]}}, Instr[31:25], Instr[11:7]}   // S-type (stores)
                // 
-      2'b11:   // J-type (jal)
+      2'b10: immext = {{20{Instr[31]}}, Instr[7], Instr[30:25], Instr[11:8], 1’b0}   // B-type (branches)
+               // 
+      2'b11: immext = {{12{Instr[31]}}, Instr[19:12], Instr[20], Instr[30:21], 1’b0}   // J-type (jal)
       default: immext = 32'bx; // undefined
     endcase             
 endmodule
